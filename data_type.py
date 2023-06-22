03634b822ba7d37597c7d74e293fef9ebaf0f7ca
tushar7783 <tusharkr43@gmail.com>
2023-06-22 21:39:05 +0530

    first commit

---
+++ data_type.py
@@ -0,0 +1,17 @@
+num=2.5
+print(type(num))
+num=5
+print(type(num))
+num=7+8j
+print(type(num))
+num=True
+print(type(num))
+a=5.5
+
+b=int(a)
+print(b)
+k=float(b)
+print(k)
+c=complex(b,k)
+print(c)
+
---
+++ variable.py
@@ -0,0 +1,12 @@
+num=5
+ans=id(num)   #id is used to find address
+print(ans)
+name='tushar'
+ans=id(name)
+print(ans)
+a=8
+b=a
+print(id(a) )
+print(id(b))  #both adress are same so in python both point to same adress if assign 
+print(type(name))
+print(type(num)) #it tell us the data type name 
